bbca11f4feef3adcce0ea1a4bc79eb206fcf10fb
Fahad Ahmad Khan <fahadahmad.dev@gmail.com>
2019-09-28 18:17:06 +0500

    memood's

--- compairing-arrays.js
+++ compairing-arrays.js
@@ -1,27 +1,33 @@
-var num = 2;
+var num = 4;
 var arr1 = [];
 var arr2 = [];
+var msg;
 var i;
-debugger;
 for(i = 0; i < num; i++) {
     var n=i+1;
     arr1[i] = Number(prompt("First Array - Number #" + n + ": "));
 }
+
 for(i = 0; i < num; i++) {
     var n=i+1;
     arr2[i] = Number(prompt("Second Array - Number #" + n + ": "));
 }
+
 for(i = 0; i < num; i++) {
-    if(arr1[i] === arr2[i] ){
-        var equal = "First Array : " + arr1 + '\n' + "Second Array : " + arr2 + '\n' + "Arrays are Equal";
-       alert(equal);
+    if(arr1[i] !== arr2[i] ){
+        break;
     }
-    else{
-        var notEqual = "First Array : " + arr1 + '\n' + "Second Array : " + arr2 + '\n' + "Arrays are not Equal";
-        alert(notEqual);
-    }
-    break;
 }
+
+var inputs = "First Array : " + arr1 + '\n' + "Second Array : " + arr2 + '\n';
+
+if (i === num) {
+    msg = 'Arrays are Equal';
+} else {
+    msg='Arrays are not Equal';
+}
+
+alert( inputs + msg );
 
 
 
