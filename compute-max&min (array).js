0239f9ed2b15d7098b0bde10c67883df894dba70
Fahad Ahmad Khan <fahadahmad.dev@gmail.com>
2019-09-30 01:55:51 +0500

    solved the min error

--- compute-max&min (array).js
+++ compute-max&min (array).js
@@ -1,30 +1,35 @@
-var num = 4;
+var num = 3;
 var arr = [];
 var i;
 var sum = 0;
 var avg;
-var val = 0;
-for(i = 0; i < num; i++) {
+
+    for (i = 0; i < num; i++) {
     var n=i+1;
     arr[i] = Number(prompt("Number #" + n + ": "));
 }
-for(i = 0; i < num; i++){
-    if(arr[i]>val){
-        val=arr[i]
-    }
-}
-var max=val;
-for(i = 0; i < num; i++){
-    if(arr[i]<val){
-        val=arr[i]
-    }
-}
-var min=val;
 
-for(i = 0; i < num; i++) {
-    sum = sum + arr[i];
+    var max = arr[0]
+    var min = arr[0]
+
+    for (i = 0; i < num; i++) {
+        if(arr[i]>max){
+            max=arr[i]
+        }
+    }
+
+    for (i = 0; i < num; i++) {
+    if(arr[i]<min){
+        min=arr[i]
+    }
 }
+
+    for (i = 0; i < num; i++) {
+       sum= sum + arr[i];
+    }
+
     avg = sum/num;
+
     alert("Numbers : " + arr + '\n' 
     + "Max Value : " + max + '\n' 
     + "Min Value :" + min + '\n'
@@ -52,3 +57,33 @@ for(i = 0; i < num; i++) {
 // + "Min Value : " + Math.min(...arr) + '\n' 
 // + "Sum Of Numbers: " + sum + '\n' 
 // + "Average : " + avg);
+
+// function maxNumber(){
+//     for(i = 0; i < num; i++){
+//         if(arr[i]>val){
+//             val=arr[i]
+//         }
+//     }
+//     return val;
+// }
+// var max=val;
+
+// // for(i = 0; i < num; i++){
+// //     if(arr[i]<val){
+// //         val=arr[i]
+// //     }
+// // }
+// // var min=val;
+
+// function addition(){
+//     for(i = 0; i < num; i++) {
+//        sum= sum + arr[i];
+//     }
+//     return sum;
+// }
+//     avg = sum/num;
+//     alert("Numbers : " + arr + '\n' 
+//     + "Max Value : " + maxNumber() + '\n' 
+//     + "Min Value :" + min + '\n'
+//     + "Sum Of Numbers: " + addition() + '\n' 
+//     + "Average : " + avg);
