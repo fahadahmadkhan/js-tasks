887627d70596fd1c61f069ff19cb89058153f7d6
Fahad Ahmad Khan <fahadahmad.dev@gmail.com>
2019-09-30 22:43:46 +0500

    code formated

--- compute-max&min (array).js
+++ compute-max&min (array).js
@@ -1,37 +1,37 @@
-var arr = [];
-    for (var i = 0; i < 5; i++) {
-    arr[i] = Number(prompt("Please Enter Number " + (i+1)+ ": "));
+var input = [];
+for (var i = 0; i < 5; i++) {
+    input[i] = Number(prompt("Please Enter Number " + (i + 1) + ": "));
 }
 
-function maxNumber(arr){
-    var max=arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        if(arr[i]>max){
-            max=arr[i]
+function maxNumber() {
+    var max = input[0];
+    for (var i = 0; i < input.length; i++) {
+        if (input[i] > max) {
+            max = input[i]
         }
     }
     return max;
 }
 
-function minNumber(arr){
-    var min=arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        if(arr[i]<min){
-            min=arr[i]
+function minNumber() {
+    var min = input[0];
+    for (var i = 0; i < input.length; i++) {
+        if (input[i] < min) {
+            min = input[i]
         }
     }
     return min;
 }
 
-function addition(arr){
+function addition() {
     var sum = 0;
-    for(var i = 0; i < arr.length; i++) {
-       sum= sum + arr[i];
+    for (var i = 0; i < input.length; i++) {
+        sum = sum + input[i];
     }
     return sum;
 }
-    alert("Numbers : " + arr + '\n' 
-    + "Max Value : " + maxNumber() + '\n' 
-    + "Min Value :" + minNumber() + '\n'
-    + "Sum Of Numbers: " + addition() + '\n' 
-    + "Average : " + addition()/arr.length);
+alert("Numbers : " + input + '\n' +
+    "Max Value : " + maxNumber(input) + '\n' +
+    "Min Value :" + minNumber(input) + '\n' +
+    "Sum Of Numbers: " + addition(input) + '\n' +
+    "Average : " + addition(input) / input.length);
