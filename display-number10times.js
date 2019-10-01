0de5f637d44a116fc89d637dc51578bd0a61e472
fahadahmadkhan <fahadahmadkhan@github.com>
2019-10-01 09:19:50 +0000

    code formated

--- display-number10times.js
+++ display-number10times.js
@@ -1,6 +1,6 @@
 var num = prompt("Please enter a number:");
 var result = 'Your Numbers are ' + '\n';
-for(var i=0; i<10; i++){
-    var result = result +   (i+1) + " : " + num + '\n';
+for (var i = 0; i < 10; i++) {
+    var result = result + (i + 1) + " : " + num + '\n';
 }
 alert(result)
