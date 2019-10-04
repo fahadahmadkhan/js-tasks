7d7683489c64f4249e8f8eb6769002885c0c9244
fahadahmadkhan <fahadahmad.dev@gmail.com>
2019-10-04 22:56:04 +0500

    wip

--- 18. [2D Arrays] matrix-multiplicaton.js
+++ 18. [2D Arrays] matrix-multiplicaton.js
@@ -1,7 +1,20 @@
-var matrix1 = [[1, 2], [3, 4]],
-    matrix2 = [[5, 6], [7, 8]];
+var matrix1 = [[1, 2], [3, 4]]
+var matrix2 = [[5, 6], [7, 8]]
+var result = [];
 
-function multiplyMatrices(matrix1, matrix2) {
-    var result = [];
-    for (var i = 0; i < matrix1.length)
+function multiplyMatrices() {
+    for (var i = 0; i < 2; i++) {
+        result[i] = [];
+        for (var j = 0; j < 2; j++) {
+            var sum = 0;
+            for (var k = 0; k < 2; k++) {
+                sum += matrix1[i][k] * matrix2[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    return result;
 }
+var matricesResult = multiplyMatrices();
+
+alert(matricesResult);
