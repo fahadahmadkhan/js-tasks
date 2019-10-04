d049ea9e2de374ae2bd3562fd79cb41a6055e7d6
fahadahmadkhan <fahadahmad.dev@gmail.com>
2019-10-04 22:58:18 +0500

    variable name changed

--- 18. [2D Arrays] matrix-multiplicaton.js
+++ 18. [2D Arrays] matrix-multiplicaton.js
@@ -1,20 +1,20 @@
 var matrix1 = [[1, 2], [3, 4]]
 var matrix2 = [[5, 6], [7, 8]]
-var result = [];
+var product = [];
 
 function multiplyMatrices() {
     for (var i = 0; i < 2; i++) {
-        result[i] = [];
+        product[i] = [];
         for (var j = 0; j < 2; j++) {
             var sum = 0;
             for (var k = 0; k < 2; k++) {
                 sum += matrix1[i][k] * matrix2[k][j];
             }
-            result[i][j] = sum;
+            product[i][j] = sum;
         }
     }
-    return result;
+    return product;
 }
-var matricesResult = multiplyMatrices();
+var matricesProduct = multiplyMatrices();
 
-alert(matricesResult);
+alert(matricesProduct);
