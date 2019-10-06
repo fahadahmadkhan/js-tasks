b33901536882d1a1270564fb17324bfd9842ea43
fahadahmadkhan <fahadahmad.dev@gmail.com>
2019-10-07 02:38:27 +0500

    code changed + improved

--- 14. [Arrays] compairing-arrays.js
+++ 14. [Arrays] compairing-arrays.js
@@ -1,19 +1,14 @@
-var num = 5;
 var arr1 = [];
 var arr2 = [];
-var msg;
-var i;
-for (i = 0; i < num; i++) {
-    var n = i + 1;
-    arr1[i] = Number(prompt("First Array - Number #" + n + ": "));
+for (var i = 0; i < 5; i++) {
+    arr1[i] = Number(prompt("First Array - Number #" + (i+1) + ": "));
 }
 
-for (i = 0; i < num; i++) {
-    var n = i + 1;
-    arr2[i] = Number(prompt("Second Array - Number #" + n + ": "));
+for (var i = 0; i < 5; i++) {
+    arr2[i] = Number(prompt("Second Array - Number #" + (i+1) + ": "));
 }
 
-for (i = 0; i < num; i++) {
+for (var i = 0; i < 5; i++) {
     if (arr1[i] !== arr2[i]) {
         break;
     }
@@ -22,9 +17,9 @@ for (i = 0; i < num; i++) {
 var inputs = "First Array : " + arr1 + '\n' + "Second Array : " + arr2 + '\n';
 
 if (i === num) {
-    msg = 'Arrays are Equal';
+    var msg = 'Arrays are Equal';
 } else {
-    msg = 'Arrays are not Equal';
+    var msg = 'Arrays are not Equal';
 }
 
 alert(inputs + msg);
